6acb77bef1a2b7aebda01aa5af39658fced7b03d
morganfula <morgan.fula@me.com>
2021-02-10 11:09:39 +0100

    image transition

---
+++ script.js
@@ -0,0 +1,29 @@
+const sliderContainer = document.querySelector('.slider__container');
+const rightSlide = document.querySelector('.right-slide');
+const leftSlide = document.querySelector('.left-slide');
+const upButton = document.querySelector('.up-button');
+const downButton = document.querySelector('.down-button');
+const slideLength = rightSlide.querySelectorAll('div').length;
+
+let ActiveSlideIndex = 0;
+
+leftSlide.style.top = `-${(slideLength - 1) * 100}vh`;
+
+upButton.addEventListener('click', () => changeSlide('up'));
+downButton.addEventListener('click', () => changeSlide('down'));
+
+const changeSlide = (direction) => {
+  const sliderHeight = sliderContainer.clientHeight;
+
+  if (direction === 'up') {
+    ActiveSlideIndex++;
+
+    if (ActiveSlideIndex > slideLength - 1) {
+      ActiveSlideIndex = 0;
+    }
+  }
+
+  rightSlide.style.transform = `translateY(-${
+    ActiveSlideIndex * sliderHeight
+  }px)`;
+};
